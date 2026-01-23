c4c90718cc6badbb72d5d8275d227a2f6b916bd3
PavanKumar <pavankumar.chittiboina@oldowaninnovations.com>
2026-01-23 15:25:15 +0530

    feat: implement toss flow and match initialization

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -8,6 +8,10 @@ import PlayerEntry from "../app/team-setup/PlayerEntryScreen";
 import RoleSelection from "../app/team-setup/RoleSelectionScreen";
 import SquadReview from "../app/team-setup/SquadReviewScreen";
 import TeamSetup from "../app/team-setup/TeamNameScreen";
+import MatchStart from "../app/toss/MatchStartScreen";
+import OpeningPlayers from "../app/toss/OpeningPlayersScreen";
+import TossDecision from "../app/toss/TossDecisionScreen";
+import Toss from "../app/toss/TossScreen";
 
 export type RootStackParamList = {
   Splash: undefined;
@@ -19,6 +23,10 @@ export type RootStackParamList = {
   PlayerEntry:undefined;
   SquadReview:undefined;
   RoleSelection:undefined;
+  TossDecision:undefined;
+  Toss:undefined;
+  OpeningPlayers:undefined;
+  MatchStart:undefined;
 
 };
 
@@ -37,6 +45,11 @@ export default function AppNavigator() {
         <Stack.Screen name="PlayerEntry" component={PlayerEntry}/>
         <Stack.Screen name="SquadReview" component={SquadReview}/>
         <Stack.Screen name="RoleSelection" component={RoleSelection}/>
+        <Stack.Screen name="Toss" component={Toss}/>
+        <Stack.Screen name="TossDecision" component={TossDecision}/>
+        <Stack.Screen name="OpeningPlayers" component={OpeningPlayers}/>
+        <Stack.Screen name="MatchStart" component={MatchStart}/>
+
       </Stack.Navigator>
     
   );
---
+++ src/types/matchState.ts
@@ -0,0 +1,7 @@
+export interface MatchState {
+  tossWinnerTeamId?: string;
+  battingFirstTeamId?: string;
+  strikerId?: string;
+  nonStrikerId?: string;
+  bowlerId?: string;
+}
